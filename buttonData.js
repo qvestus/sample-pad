b4f6e914f811a26582f33693cf34334ab1c39341
kristoffer@live.no <kristoffer@live.no>
2023-04-20 16:09:39 +0200

    fixez

--- buttonData.js
+++ buttonData.js
@@ -1,16 +1,16 @@
 const buttonData = [
     { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
-    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
-    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
-    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
-    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
-    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
-    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
-    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
-    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
-    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
-    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
-    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 2' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 3' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 4' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 5' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 6' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 7' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 8' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 9' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 10' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 11' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 12' },
     // Add more button data objects here
   ];
 

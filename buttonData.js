664984fcdd958935f10046abf27738704a7c7350
Kristoffer Iversen <kristoffer@live.no>
2023-04-20 03:07:09 +0200

    fix

--- buttonData.js
+++ buttonData.js
@@ -13,4 +13,5 @@ const buttonData = [
     { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
     // Add more button data objects here
   ];
-  
+
+  export { buttonData };

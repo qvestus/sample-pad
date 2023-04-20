0203bdd90aba265f1042eaf4cfb250ef5d961a95
Kristoffer Iversen <kristoffer@live.no>
2023-04-20 02:57:31 +0200

    Testings

---
+++ buttonData.js
@@ -0,0 +1,17 @@
+const buttonData = [
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
+    { imgSrc: 'images/alsaker.jpg', soundSrc: 'audio/sound1.mp3', label: 'Sound 1' },
+    // Add more button data objects here
+  ];
+
+  module.exports = buttonData;
--- scripts.js
+++ scripts.js
@@ -1,4 +1,30 @@
-function playSound(audioFile) {
-    const audio = new Audio(audioFile);
-    audio.play();
-  }
+const buttonData = require('./button-data.js');
+
+const container = document.getElementById('button-container');
+
+buttonData.forEach((button) => {
+  const buttonElement = document.createElement('button');
+  buttonElement.classList.add('button');
+  buttonElement.addEventListener('click', () => playSound(button.soundSrc));
+
+  const buttonContent = document.createElement('div');
+  buttonContent.classList.add('button-content');
+
+  const imgElement = document.createElement('img');
+  imgElement.src = button.imgSrc;
+  imgElement.alt = button.label;
+
+  const labelElement = document.createElement('span');
+  labelElement.classList.add('button-label');
+  labelElement.textContent = button.label;
+
+  buttonContent.appendChild(imgElement);
+  buttonContent.appendChild(labelElement);
+  buttonElement.appendChild(buttonContent);
+  container.appendChild(buttonElement);
+});
+
+function playSound(soundSrc) {
+  const audio = new Audio(soundSrc);
+  audio.play();
+}

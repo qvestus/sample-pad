3b9420357e9da86fca286e4efa339af10914ef14
kristoffer@live.no <kristoffer@live.no>
2023-04-20 16:40:59 +0200

    bugfix

--- scripts.js
+++ scripts.js
@@ -23,3 +23,8 @@ buttonData.forEach((button) => {
   buttonElement.appendChild(buttonContent);
   container.appendChild(buttonElement);
 });
+
+function playSound(soundSrc) {
+  const audio = new Audio(soundSrc);
+  audio.play();
+}

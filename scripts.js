6357ca53d2f37631d4d8a7524b17331bac84bed2
kristoffer@live.no <kristoffer@live.no>
2023-04-21 22:06:15 +0200

    wiggle

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ import { buttonData } from './buttonData.js';
 const container = document.querySelector('#button-container');
 
 const buttonTemplate = ({ imgSrc, label }) => `
-  <button class="button">
+  <button class="button button-initial">
     <div class="button-content">
       <img src="${imgSrc}" alt="${label}">
       <span class="button-label">${label}</span>
@@ -14,7 +14,7 @@ const buttonTemplate = ({ imgSrc, label }) => `
 buttonData.forEach((button) => {
   const buttonElement = document.createRange().createContextualFragment(buttonTemplate(button)).querySelector('button');
   
-  buttonElement.addEventListener('click', () => playSound(button));
+  buttonElement.addEventListener('click', () => playSound(button, buttonElement));
 
   container.appendChild(buttonElement);
 
@@ -22,11 +22,17 @@ buttonData.forEach((button) => {
   button.audio.preload = 'auto';
 });
 
-function playSound(button) {
+function playSound(button, buttonElement) {
   button.audio.currentTime = 0;
   button.audio.play();
+
+  buttonElement.classList.remove('button-initial');
+  buttonElement.classList.add('wiggle');
+  button.audio.addEventListener('ended', () => {
+    buttonElement.classList.remove('wiggle');
+  });
 }
- 
+
 window.addEventListener("load", () => {
   document.body.classList.add("loaded");
 });
